f09b9e77f59784f0f8bc249446b1c912439467c3
Roksana Zalesskaia <roksanazalesskaia@gmail.com>
2025-04-29 18:27:29 +0300

    Added tests for null, empty, fake paths. Fixed access to file on using method getResources

--- src/test/java/org/mentor/service/FileOrderServiceTest.java
+++ src/test/java/org/mentor/service/FileOrderServiceTest.java
@@ -1,6 +1,5 @@
 package org.mentor.service;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 import org.mentor.exception.ReadFileException;
@@ -8,8 +7,11 @@ import org.mentor.exception.WriteFileException;
 import org.mentor.model.OrderReport;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,14 +23,10 @@ class FileOrderServiceTest {
     @TempDir
     Path tempDir;
 
-    @BeforeEach
-    void setUp() throws IOException {
-        tempDir = Files.createTempDirectory("tempDir");
-    }
-
     @Test
-    void readShouldReturnsLinesIfFileExists() throws IOException {
-        Path testFile = Path.of("src/test/resources/testFile.txt");
+    void readShouldReturnsLinesIfFileExists() throws IOException, URISyntaxException {
+        URL resource = getClass().getResource("/testFile.txt");
+        Path testFile = Paths.get(resource.toURI());
         List<String> expectedLines = List.of("Line1", "Line2");
         Files.write(testFile, expectedLines);
         List<String> actualLines = fileOrderService.read(testFile.toString());
@@ -58,8 +56,8 @@ class FileOrderServiceTest {
             for (int i = 0; i < orderReports.size(); i++) {
                 assertEquals(orderReports.get(i).toString(), actualLines.get(i));
             }
-        } finally {
-            Files.delete(outputFile);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
@@ -68,5 +66,55 @@ class FileOrderServiceTest {
         List<OrderReport> orderReports = List.of(new OrderReport("CompanyA", 111));
         assertThrows(WriteFileException.class,
                 () -> fileOrderService.write("/non_writable_directory/output.txt", orderReports));
+    }
+
+    @Test
+    void writeShouldThrowExceptionWhenNullPath() {
+        List<OrderReport> orderReports = List.of(
+                new OrderReport("CompanyA", 111),
+                new OrderReport("CompanyB", 222)
+        );
+        String nullPath = null;
+        assertThrows(NullPointerException.class,
+                () -> fileOrderService.write(nullPath, orderReports));
+    }
+
+    @Test
+    void writeShouldThrowExceptionWhenEmptyPath() {
+        List<OrderReport> orderReports = List.of(
+                new OrderReport("CompanyA", 111),
+                new OrderReport("CompanyB", 222)
+        );
+        String emptyPath = "";
+        assertThrows(WriteFileException.class,
+                () -> fileOrderService.write(emptyPath, orderReports));
+    }
+
+    @Test
+    void writeShouldThrowExceptionWhenNonExistentPath() {
+        List<OrderReport> orderReports = List.of(
+                new OrderReport("CompanyA", 111),
+                new OrderReport("CompanyB", 222)
+        );
+        String nonExistentPath = "/non/existent/path/output.txt";
+        assertThrows(WriteFileException.class,
+                () -> fileOrderService.write(nonExistentPath, orderReports));
+    }
+
+    @Test
+    void writeShouldThrowExceptionWhenNullOrderReports() {
+        List<OrderReport> orderReports = null;
+        String path = "output.txt";
+        assertThrows(NullPointerException.class,
+                () -> fileOrderService.write(path, orderReports));
+    }
+
+    @Test
+    void writeShouldCreateFileAndWriteWithoutExceptionWhenEmptyOrderReports() {
+        List<OrderReport> orderReports = List.of();
+        String path = "output_emptyOrdersList.txt";
+        fileOrderService.write(path, orderReports);
+        Path filePath = Path.of(path);
+        assertTrue(Files.exists(filePath));
     }
 }
